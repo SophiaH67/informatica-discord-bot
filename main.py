6da4f32d82ada9b061db2b5c0733d413d86c6bae
marnixah <marnix@hagehoeve.com>
2021-04-16 11:24:20 +0200

    feat: added command loader

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import discord
 import os
 from dotenv import load_dotenv
-
+import glob
+import importlib
+from pathlib import Path
 load_dotenv()
 
 client = discord.Client()
+cmds = {}
+prefix = os.getenv("prefix")
+def load_extensions():
+    for commandFile in glob.glob("./cmds/*.py"):
+        commandFilePath = Path(commandFile)
+        command = __import__("cmds.{}".format(commandFilePath.stem), fromlist=["get_aliases", "run"])
+        for alias in command.get_aliases():
+            cmds[alias] = command.run
 
 @client.event
 async def on_ready():
     print('Logged in as {}'.format(client.user))
+    load_extensions()
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    if not message.content.startswith(prefix):
+        return
+    args = message.content[len(prefix):].split(" ")
+    command = args.pop(0)
+    output = cmds[command](args, client)
+    if output:
+        await message.channel.send(str(output))    
 
 client.run(os.getenv("token"))
