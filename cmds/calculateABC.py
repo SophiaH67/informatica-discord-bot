4fa200f101be39c78375063a984e1ec01784d98d
marnixah <marnix@hagehoeve.com>
2021-04-16 11:25:08 +0200

    feat: added command to solve an abc formula

---
+++ cmds/calculateABC.py
@@ -0,0 +1,28 @@
+from sympy import symbols, Eq, solve
+
+
+def calculateABC(A, B, C):
+    x = symbols('x')
+    equation = Eq(A*x**2+B*x+C, 0)
+    return solve(equation)[0]
+
+def get_aliases():
+    return ["calculateabc"]
+
+def run(args, bot):
+    try:
+        a = float(args[0])
+    except:
+        return "a is not a correct number"
+
+    try:
+        b = float(args[1])
+    except:
+        return "b is not a correct number"
+
+    try:
+        c = float(args[2])
+    except:
+        return "c is not a correct number"
+
+    return "```{}```".format(calculateABC(a,b,c))
