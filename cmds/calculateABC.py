2aba711e60f1b0b405990f9c9124f3a8e8b7d388
marnixah <marnix@hagehoeve.com>
2021-04-16 18:15:59 +0200

    fix: removed leading zero's from all numbers in abc calc

--- cmds/calculateABC.py
+++ cmds/calculateABC.py
@@ -1,10 +1,13 @@
 from sympy import symbols, Eq, solve
 import discord
 
+def floatToString(flt):
+    return ('%.15f' % flt).rstrip('0').rstrip('.')
+
 def calculateABC(A, B, C):
     x = symbols('x')
     equation = Eq(A*x**2+B*x+C, 0)
-    return " or ".join(('%.15f' % v).rstrip('0').rstrip('.') for v in solve(equation))
+    return " or ".join(floatToString(v) for v in solve(equation))
 
 def get_aliases():
     return ["calculateabc"]
@@ -25,5 +28,5 @@ def run(args, bot):
     except:
         return "c is not a correct number"
     e = discord.Embed()
-    e.add_field(name="{}x^2 + {}x + {}".format(str(a),str(b),str(c)), value=calculateABC(a,b,c))
+    e.add_field(name="{}x^2 + {}x + {} = 0".format(floatToString(a),floatToString(b),floatToString(c)), value=calculateABC(a,b,c))
     return e
