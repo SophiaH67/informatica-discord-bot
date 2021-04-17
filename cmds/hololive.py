41717ed49816d6587887d31ef9f2f7585ff6775f
marnixah <marnix@hagehoeve.com>
2021-04-17 23:23:30 +0200

    feat: added error message for hololive schedule

--- cmds/hololive.py
+++ cmds/hololive.py
@@ -6,6 +6,11 @@ def get_aliases():
 
 def run(args, bot):
   e = discord.Embed(title="Hololive schedule")
-  for stream in bot.hololive_schedule:
-    e.add_field(name=stream["title"], value="[{}]({})\n{}".format(stream["talent"], stream["url"],stream["datetime"].isoformat()))
+  try:
+    e.color = 0x00FF00
+    for stream in bot.hololive_schedule:
+      e.add_field(name=stream["title"], value="[{}]({})\n{}".format(stream["talent"], stream["url"],stream["datetime"].isoformat()))
+  except AttributeError:
+    e.add_field(name="Error", value="There was an error with the hololive API")
+    e.color = 0xFF0000
   return e
