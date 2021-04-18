0d017864d146edc2d35e52aeb455f03e1e53d8f8
marnixah <marnix@hagehoeve.com>
2021-04-18 15:45:20 +0200

    feat: made help message not be inline

--- cmds/help.py
+++ cmds/help.py
@@ -9,6 +9,6 @@ def run(args, bot):
     e = discord.Embed(title="Help")
     e.color = 0x0000FF
     for i, (command, help_message) in enumerate(bot.help.items()):
-        e.add_field(name=command, value=help_message)
+        e.add_field(name=command, value=help_message, inline=False)
     return e
     
