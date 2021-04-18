fbd6f1b83bd6d431bb158f6b2508db7c2e0c6edb
marnixah <marnix@hagehoeve.com>
2021-04-18 15:34:54 +0200

    feat: added help message to reload command

--- cmds/reload.py
+++ cmds/reload.py
@@ -1,6 +1,9 @@
 def get_aliases():
     return ["reload","refresh","werk"]
 
+def get_help():
+    return "Reloads all commands"
+
 def run(args, bot):
     bot.reload()
     return "Reloading all commands"
