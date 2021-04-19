5fe329be2528e53309495bb348c94fc0e950f088
marnixah <marnix@hagehoeve.com>
2021-04-19 20:02:40 +0200

    feat: changed way of displaying streams

--- cmds/hololive.py
+++ cmds/hololive.py
@@ -1,5 +1,7 @@
 import discord
 import datetime
+import arrow
+from typing import List
 
 def get_aliases():
   return ["hololive","schedule","holoschedule"]
@@ -10,10 +12,37 @@ def get_help():
 def run(args, bot):
   e = discord.Embed(title="Hololive schedule")
   try:
-    e.color = 0x00FF00
     for stream in bot.hololive_schedule:
-      e.add_field(name=stream["title"], value="[{}]({})\n{}".format(stream["talent"], stream["url"],stream["datetime"].isoformat()))
+      pass
   except AttributeError:
     e.add_field(name="Error", value="There was an error with the hololive API")
     e.color = 0xFF0000
+    return e
+
+  e.color = 0x00FF00
+  entries = []
+  current_day = -1
+  current_time = datetime.datetime.now()
+  
+  for stream in bot.hololive_schedule:
+    date: datetime.datetime = stream["datetime"]
+    time = arrow.Arrow(date.year, date.month, date.day, date.hour, date.minute, date.second)
+    
+    if not current_day == date.day:
+      current_day = date.day
+      if not len(entries) == 0:
+        entries.append("")
+      diff = datetime.datetime(date.year, date.month, date.day, current_time.hour, current_time.minute, current_time.second) - current_time
+      if diff.days < 1:
+        entries.append("**today**")
+      elif diff.days == 1:
+        entries.append("**tomorrow**")
+      else:
+        entries.append("**in {} days".format(diff.days))
+
+    entries.append("**[{}]({})**".format(stream["title"], stream["url"]))
+    entries.append(stream["talent"])
+    entries.append(time.humanize())
+      
+  e.description = "\n".join(entries)
   return e
