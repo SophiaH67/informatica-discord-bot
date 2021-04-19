0fd2b018ec122b8d421be53ce50455cef2257987
marnixah <marnix@hagehoeve.com>
2021-04-19 19:28:09 +0200

    fix: removed brazil bg task
    This task was override the on_message defined in main.py

--- bg/brazil.py
+++
@@ -1,9 +0,0 @@
-import discord
-import time
-import random
-
-def start(bot: discord.Client):
-  @bot.event
-  async def on_message(message):
-    if random.randrange(0,10) > 8:
-      await message.channel.send("Go to brazil {} you stinky monkey".format(message.author.mention))
