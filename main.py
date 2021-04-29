071121096250204a21eb81805a2865da8a579322
marnixah <marnix@hagehoeve.com>
2021-04-29 09:08:27 +0200

    fix: removed unused variables

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from urllib.parse import unquote
 import threading
 load_dotenv()
 
-cmds = {}
 prefix = unquote(os.getenv("prefix"))
 client = commands.Bot(command_prefix=prefix)
 def load_commands():
@@ -25,28 +24,10 @@ def load_background_tasks():
         thread.daemon = True
         thread.start()
 
-client.reload = load_commands
-
 @client.event
 async def on_ready():
     print('Logged in as {}'.format(client.user))
     load_commands()
     load_background_tasks()
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if not message.content.startswith(prefix):
-        return
-    args = message.content[len(prefix):].split(" ")
-    command = args.pop(0)
-    if not command in cmds:
-        return
-    output = cmds[command](args, client)
-    if type(output) is str:
-        await message.channel.send(str(output))
-    elif type(output) is discord.Embed:
-        await message.channel.send(embed=output)
 
 client.run(os.getenv("token"))
