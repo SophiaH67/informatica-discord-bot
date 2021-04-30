48bb14b544e75299af48d73665c5c5a008e0159f
marnixah <marnix@hagehoeve.com>
2021-04-30 23:17:13 +0200

    chore(gif.py): added types

--- cmds/gif.py
+++ cmds/gif.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from discord import Embed
+from discord.ext.commands.context import Context
 import TenGiphPy
 import os
 
@@ -7,14 +8,16 @@ token = os.getenv("TENOR_TOKEN")
 g = TenGiphPy.Tenor(token=token)
 
 @commands.command(name="gif",aliases=["search"], help="Searches and sends a GIF with query")
-async def run(ctx, *query):
+async def run(ctx: Context, *query: tuple[str, ...]):
+    print(type(query))
+    full_query = " ".join(''.join(word) for word in query)
     try:
-        url = g.random(tag=" ".join(query) )
+        url = g.random(tag=full_query)
     except KeyError:
         return await ctx.send("No tenor API token was provided. This functionality will not work")
     except IndexError:
-        return await ctx.send("Couldn't find any gifs with the query \"{}\"".format(" ".join(query)))
-    e = Embed(title=" ".join(query))
+        return await ctx.send("Couldn't find any gifs with the query \"{}\"".format(full_query))
+    e = Embed(title=full_query)
     e.set_image(url=url)
 
     return await ctx.send(embed=e)
