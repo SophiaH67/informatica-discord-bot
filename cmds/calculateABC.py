ed47455e9db7d967670f80de2dfe397cc73b5ac3
marnixah <marnix@hagehoeve.com>
2021-05-01 16:19:12 +0200

    chore: added types to calculateABC.py

--- cmds/calculateABC.py
+++ cmds/calculateABC.py
@@ -1,7 +1,6 @@
 from sympy import symbols, Eq, solve, core
 from discord.ext import commands
 from discord import Embed
-from discord.ext.commands.context import Context
 
 def floatToString(flt:float) -> str:
     return ('%.15f' % flt).rstrip('0').rstrip('.')
@@ -17,11 +16,11 @@ def calculateABC(A:float, B:float, C:float) -> str:
 
 
 @commands.command(name="abc", aliases=["abcformula","calculate","solve"], help="Calculates quadratic formula")
-async def run(ctx: Context, A: float, B:float, C:float):
+async def run(ctx: commands.context.Context, A: float, B:float, C:float):
     e = Embed()
     e.add_field(name="{}x^2 + {}x + {} = 0".format(floatToString(A),floatToString(B),floatToString(C)), value=calculateABC(A,B,C))
     e.color = 0x00FF00
     return await ctx.send(embed=e)
 
-def setup(bot):
+def setup(bot: commands.bot.Bot):
     bot.add_command(run)
