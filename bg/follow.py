8fc242bbea0537240ba069882eb8754deb02e4bf
marnixah <marnix@hagehoeve.com>
2021-05-04 17:57:34 +0200

    feat(follow): made bot follow largest vc

---
+++ bg/follow.py
@@ -0,0 +1,43 @@
+from discord.ext import commands
+from discord.member import Member, VoiceState
+from discord.guild import Guild
+from discord.channel import VoiceChannel
+from discord.member import Member
+from typing import List
+
+class Events(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+    self.bot.connected_guilds = {}
+
+  @commands.Cog.listener()
+  async def on_voice_state_update(self, member: Member, before: VoiceState, after: VoiceState):
+    if member._user.id == self.bot.user.id:
+      return
+    await self.join_biggest_vc(member.guild)
+
+  async def join_biggest_vc(self, guild: Guild):
+    voice_channels: List[VoiceChannel] = guild.voice_channels
+    highest_count = 0
+    highest_channel = None
+    for voice_channel in voice_channels:
+      member_count = 0
+      members = voice_channel.members
+      members = (m for m in members if not (m._user.bot) )
+      for member in members:
+        member_count +=1
+      if member_count > highest_count:
+        highest_channel = voice_channel
+        
+    if(highest_channel is None):
+      if guild.id in self.bot.connected_guilds:
+        return await self.bot.connected_guilds[guild.id].disconnect()
+      return
+    if guild.id in self.bot.connected_guilds:
+      if highest_channel.id == self.bot.connected_guilds[guild.id].channel.id:
+        return
+      await self.bot.connected_guilds[guild.id].disconnect()
+    self.bot.connected_guilds[guild.id] = await highest_channel.connect()
+
+def setup(bot: commands.bot.Bot):
+  bot.add_cog(Events(bot))
