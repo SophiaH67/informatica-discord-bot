a6e8a07fdaab783f8cbb98d12e89a434aa5e94fc
marnixah <marnix@hagehoeve.com>
2021-05-06 11:19:18 +0200

    feat: added version commmand

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,7 @@ FROM python:rc-buster
 WORKDIR /app
 COPY requirements.txt ./
 RUN [ "pip", "install", "--no-cache-dir" ,"-r", "requirements.txt"]
+RUN ["apt-get", "update", "-y"]
+RUN ["apt-get", "install", "-y", "git"]
 COPY . .
 CMD [ "python", "main.py"]
---
+++ cmds/version.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+from discord import Embed
+import subprocess
+
+@commands.command(name="version", help="Gets version number")
+async def run(ctx: commands.context.Context, command:str=""):
+  commit_hash = subprocess.check_output(["git", "describe", "--always"]).decode("utf-8")
+  e = Embed(title="Current version")
+  e.color = 0xFF00FF
+  e.description = "I am currently running version [{0}](https://github.com/marnixah/informatica-discord-bot/commit/{0})".format(commit_hash)
+  return await ctx.send(embed=e)
+
+def setup(bot: commands.bot.Bot):
+  bot.add_command(run)
