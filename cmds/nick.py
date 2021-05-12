2edac38d41d2264e75b6c1f31f6cba441ee48423
marnixah <marnix@hagehoeve.com>
2021-05-12 09:49:01 +0200

    fix(nick): made nickname multi string

--- cmds/nick.py
+++ cmds/nick.py
@@ -1,13 +1,15 @@
 from discord.ext import commands
 from discord import Embed
 import subprocess
+from typing import Tuple
 
 @commands.command(name="nick", help="Changes bot nickname")
-async def run(ctx: commands.context.Context, nickname: str):
-  await ctx.guild.me.edit(nick=nickname)
+async def run(ctx: commands.context.Context, *nickname: Tuple[str, ...]):
+  nick = " ".join(nickname)
+  await ctx.guild.me.edit(nick=nick)
   e = Embed(title="Success!")
   e.color = 0x00FF00
-  e.description = f"Succesfully set nickname to {nickname}"
+  e.description = f"Succesfully set nickname to {nick}"
   return await ctx.send(embed=e)
 
 def setup(bot: commands.bot.Bot):
