d492b4af2467f50426396933003fc79c344ebb10
marnixah <marnix@hagehoeve.com>
2021-05-12 09:43:12 +0200

    feat(version): added hostname to version command

--- cmds/version.py
+++ cmds/version.py
@@ -1,11 +1,12 @@
 from discord.ext import commands
 from discord import Embed
 import subprocess
+from socket import gethostname
 
 @commands.command(name="version", help="Gets version number")
 async def run(ctx: commands.context.Context):
   commit_hash = subprocess.check_output(["git", "describe", "--always"]).decode("utf-8")
-  e = Embed(title="Current version")
+  e = Embed(title=gethostname())
   e.color = 0xFF00FF
   e.description = "I am currently running version [{0}](https://github.com/marnixah/informatica-discord-bot/commit/{0})".format(commit_hash)
   return await ctx.send(embed=e)
