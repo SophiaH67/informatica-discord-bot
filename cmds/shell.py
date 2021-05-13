0452a6927ffbd17c83d7e0187ca2f750980cff26
marnixah <marnix@hagehoeve.com>
2021-05-13 15:39:41 +0200

    feat(shell): added shell command for learning python

---
+++ cmds/shell.py
@@ -0,0 +1,60 @@
+from typing import Tuple
+from discord.ext import commands
+from discord import Embed
+
+bot:commands.bot.Bot = None
+
+@commands.command(name="shell", help="Opens a python interactive shell")
+@commands.has_permissions(administrator=True)
+async def run(ctx: commands.context.Context):
+  await ctx.send(">>>")
+  def check(message):
+    return message.author.id == ctx.author.id
+  while True:
+    try:
+      msg = await bot.wait_for('message', check=check, timeout=30)
+    except TimeoutError:
+      return await ctx.send("Exited shell(timeout)")
+    output = await exec_code(msg.content)
+    if not output:
+      await ctx.send("Exited shell succesfully")
+      break
+    messages = []
+    current_message = ""
+    words = output.split()
+    for i in range(len(words)):
+      word = words[i]
+      if len(current_message + word) >= 2000:
+        messages.append(current_message)
+        current_message = ""
+      current_message += "" if len(current_message) == 0 else " "
+      current_message += word
+      if i == (len(words)-1):
+        messages.append(current_message)
+    for message in messages:
+      await ctx.send(message)
+
+async def exec_code(code) -> str:
+  exitted = False
+  def exit():
+    global exitted
+    exitted = True
+  try:
+    output = eval(code)
+  except Exception as e:
+    output = str(e)
+  if len(output) == 0:
+    output = ":white_check_mark:"
+  return output if not exitted else None
+@run.error
+async def run_error(error, ctx):
+  if isinstance(error, commands.CheckFailure):
+    e = Embed(title="Permission denied!")
+    e.color = 0xFF0000
+    e.description = "You need to be an administrator to use this command"
+    await ctx.send(embed=e)
+
+def setup(client: commands.bot.Bot):
+  global bot
+  bot = client
+  client.add_command(run)
